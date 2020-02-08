5b7f9c8c4770726be4639c3b7063616ddc73cdec
zs <123>
2020-02-08 17:57:09 +0800

    2月8号

---
+++ zipkinserver/src/main/java/com/test1/ZipkinServerApplication.java
@@ -0,0 +1,13 @@
+package com.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+}
